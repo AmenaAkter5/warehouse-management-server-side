eeccc91f5595196a73d30fb1126b6bae175a7a95
unknown <latapata1952@gmail.com>
2022-05-02 00:37:04 +0600

    get item api and delete item api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // require express, cors, mongodb and dotenv to secure database pass
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -15,6 +15,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// heroku link:
+// https://pure-cliffs-64798.herokuapp.com/
 
 
 // connect with mongo database
@@ -33,7 +36,7 @@ async function run() {
         const fruitsCollection = client.db("warehouse").collection("fruits");
 
 
-        // Make API : get data from server [get all fruit]
+        // Make API : get data from server [get all fruit data]
         // link: http://localhost:5000/fruits
 
         app.get('/fruits', async (req, res) => {
@@ -41,6 +44,28 @@ async function run() {
             const cursor = fruitsCollection.find(query);
             const fruits = await cursor.toArray();
             res.send(fruits);
+        })
+
+
+        // get data : get a specific fruit item data
+        // link: http://localhost:5000/fruits/${id}
+
+        app.get('/fruits/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const fruit = await fruitsCollection.findOne(query);
+            res.send(fruit);
+        })
+
+
+        // delete data : delete a specific fruit
+        // link: http://localhost:5000/fruits/${id}
+
+        app.delete('/fruits/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await fruitsCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
